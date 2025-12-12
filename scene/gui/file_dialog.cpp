579b36a7a4bb053138ba533e41b74e275f33c52b
kobewi <kobewi4e@gmail.com>
2025-12-12 20:46:12 +0100

    Scroll to selected file on sorting change

--- scene/gui/file_dialog.cpp
+++ scene/gui/file_dialog.cpp
@@ -1553,6 +1553,10 @@ void FileDialog::_invalidate() {
 
 	update_file_list();
 
+	if (ensure_visible_after_invalidating) {
+		file_list->ensure_current_is_visible();
+		ensure_visible_after_invalidating = false;
+	}
 	is_invalidating = false;
 }
 
@@ -1660,6 +1664,7 @@ void FileDialog::_sort_option_selected(int p_option) {
 		file_sort_button->get_popup()->set_item_checked(i, (i == p_option));
 	}
 	file_sort = FileSortOption(p_option);
+	ensure_visible_after_invalidating = true;
 	invalidate();
 }
 
