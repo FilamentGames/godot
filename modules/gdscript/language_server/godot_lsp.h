4015b4d074331ca340e5f1fa034f032716c2b3cf
HolonProduction <holonproduction@gmail.com>
2026-01-31 13:40:51 +0100

    LSP: Handle clients that do not support `CompletionContext`

--- modules/gdscript/language_server/godot_lsp.h
+++ modules/gdscript/language_server/godot_lsp.h
@@ -1447,7 +1447,7 @@ struct CompletionContext {
 	/**
 	 * How the completion was triggered.
 	 */
-	int triggerKind = CompletionTriggerKind::TriggerCharacter;
+	int triggerKind = CompletionTriggerKind::Invoked;
 
 	/**
 	 * The trigger character (a single character) that has trigger code complete.
@@ -1470,7 +1470,10 @@ struct CompletionParams : public TextDocumentPositionParams {
 
 	void load(const Dictionary &p_params) {
 		TextDocumentPositionParams::load(p_params);
-		context.load(p_params["context"]);
+
+		if (p_params.has("context")) {
+			context.load(p_params["context"]);
+		}
 	}
 
 	Dictionary to_json() {
