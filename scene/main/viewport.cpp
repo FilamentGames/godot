7ec7d62333064dca648f2a51130ef656f98426bb
Rémi Verschelde <rverschelde@gmail.com>
2026-03-26 17:08:42 +0100

    Revert "Expose Control.call_gui_input"
    This reverts commit 35eabf06b9f676b2e4518cca70d825fd91e09b76.
    
    This was merged by mistake, there's no consensus yet on the implementation.

--- scene/main/viewport.cpp
+++ scene/main/viewport.cpp
@@ -1767,7 +1767,7 @@ void Viewport::_gui_call_input(Control *p_control, const Ref<InputEvent> &p_inpu
 		Control *control = Object::cast_to<Control>(ci);
 		if (control) {
 			if (control->get_mouse_filter_with_override() != Control::MOUSE_FILTER_IGNORE) {
-				control->call_gui_input(ev);
+				control->_call_gui_input(ev);
 			}
 
 			if (!control->is_inside_tree() || control->is_set_as_top_level()) {
@@ -2324,7 +2324,7 @@ void Viewport::_gui_input_event(Ref<InputEvent> p_event) {
 
 		if (gui.key_focus) {
 			if (gui.key_focus->can_process()) {
-				gui.key_focus->call_gui_input(p_event);
+				gui.key_focus->_call_gui_input(p_event);
 			}
 
 			if (is_input_handled()) {
@@ -2780,7 +2780,7 @@ void Viewport::_drop_mouse_focus() {
 			mb->set_button_index(MouseButton(i + 1));
 			mb->set_pressed(false);
 			mb->set_device(InputEvent::DEVICE_ID_INTERNAL);
-			c->call_gui_input(mb);
+			c->_call_gui_input(mb);
 		}
 	}
 }
@@ -2839,7 +2839,7 @@ void Viewport::_post_gui_grab_click_focus() {
 				mb->set_button_index(MouseButton(i + 1));
 				mb->set_pressed(false);
 				mb->set_device(InputEvent::DEVICE_ID_INTERNAL);
-				gui.mouse_focus->call_gui_input(mb);
+				gui.mouse_focus->_call_gui_input(mb);
 			}
 		}
 
@@ -2857,7 +2857,7 @@ void Viewport::_post_gui_grab_click_focus() {
 				mb->set_button_index(MouseButton(i + 1));
 				mb->set_pressed(true);
 				mb->set_device(InputEvent::DEVICE_ID_INTERNAL);
-				callable_mp(gui.mouse_focus, &Control::call_gui_input).call_deferred(mb);
+				callable_mp(gui.mouse_focus, &Control::_call_gui_input).call_deferred(mb);
 			}
 		}
 	}
