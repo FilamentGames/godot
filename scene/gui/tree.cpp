1287b1772a8fa18b5be9b1cdd0c2629829cabec3
Simon Döhl <simon.doehl@googlemail.com>
2026-04-18 21:08:11 +0200

    Free `Tree` `custom_ci` RID in destructor

--- scene/gui/tree.cpp
+++ scene/gui/tree.cpp
@@ -7502,5 +7502,6 @@ Tree::~Tree() {
 	}
 	RenderingServer::get_singleton()->free_rid(drop_indicator_ci);
 	RenderingServer::get_singleton()->free_rid(content_ci);
+	RenderingServer::get_singleton()->free_rid(custom_ci);
 	RenderingServer::get_singleton()->free_rid(header_ci);
 }
