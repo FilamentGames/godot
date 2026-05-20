697df5765dd83a35263bdfb0549e7d231645956f
Nate Marshall <nmarshall@filamentgames.com>
2026-05-20 19:45:02 -0400

    adjust webbuild script
    now requires a EMSDK env var to be set so it can auto-set the
    environment

--- minimal_web_editor.ps1
+++ minimal_web_editor.ps1
@@ -1,3 +1,21 @@
+# !/usr/bin/env pwsh
+
+# Requires a manually-set Env Var for the EMSDK Path
+if (-not $env:EMSDK) {
+    Write-Error "EMSDK environment variable is not set."
+    Write-Warning "Please set the EMSDK environment variable to the root of the Emscripten SDK Folder."
+    exit 1
+}
+
+Write-Host "Activating Emscripten At Path: $env:EMSDK`n"
+
+. "$env:EMSDK\emsdk.ps1" install latest
+. "$env:EMSDK\emsdk.ps1" activate latest
+. "$env:EMSDK\emsdk_env.ps1"
+
+Write-Host "`nEmscripten Activated`n"
+
+Write-Host "Building Godot Editor...`n"
 scons `
     platform=web `
     target=editor `
